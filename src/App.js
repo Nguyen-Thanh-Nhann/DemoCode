3d903d70c1d343134d257a8a5d98ba6532a9cc1a
Nguyen-Thanh-Nhann <bizner94@gmail.com>
2024-04-09 20:26:35 +0700

    4/9/2024

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import OrderPage from './pages/OrderPage/OrderPage'
 
-export default App;
+ function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/order" element={<OrderPage />}/>
+          <Route path="/" element={<HomePage />}/>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+export default App
