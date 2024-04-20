3f337327f728f3f079962819f76d59d4595f3b9e
TanTai <tientantai12@gmail.com>
2024-04-20 09:19:49 +0700

    xong video 8

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,34 +1,56 @@
 import React from "react";
 import TypeProduct from "../../components/TypeProduct/TypeProduct";
-import { WrapperTypeProduct } from "./style";
-import slider1 from '../../assets/images/slider1.webp'
-import slider2 from '../../assets/images/slider2.webp'
-import slider3 from '../../assets/images/slider3.webp'
+import { WrapperProducts, WrapperTypeProduct } from "./style";
+import slider1 from "../../assets/images/slider1.webp";
+import slider2 from "../../assets/images/slider2.webp";
+import slider3 from "../../assets/images/slider3.webp";
 import SliderComponent from "../../components/SliderComponent/SliderComponent";
 import CardComponent from "../../components/CardComponent/CardComponent";
-
+import NavBarComponent from "../../components/NavBarComponent/NavBarComponent";
 
 const HomePage = () => {
-  const arr = ['TV','TU LANH', 'LAPTOP']
+  const arr = ["TV", "TU LANH", "LAPTOP"];
   return (
     <>
-    <div style={{padding: '0 120px'}}>
-      <WrapperTypeProduct>
-        {arr.map((item) => {
-          return (
-          <TypeProduct name ={item} key={item}/>
-          )
-        })}
-      </WrapperTypeProduct>
+      <div style={{ padding: "0 120px" }}>
+        <WrapperTypeProduct>
+          {arr.map((item) => {
+            return <TypeProduct name={item} key={item} />;
+          })}
+        </WrapperTypeProduct>
       </div>
-      <div id="container" style={{backgroundColor: '#efefef', padding:'0 120px', height:'1000px'}}>
-      <SliderComponent arrImages={[slider1,slider2,slider3]}/>
-      <div style={{marginTop:'20px', display:'flex', alignItems :'center',gap:'20px'}}>
-        <CardComponent/>
-      </div>
-    </div>
-    </>
-  )
-}
+      <div
+        id="container"
+        style={{
+          backgroundColor: "#efefef",
+          padding: "0 120px",
+          height: "1000px",
+        }}
+      >
+        <SliderComponent arrImages={[slider1, slider2, slider3]} />
+        <div
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            alignItems: "center",
 
-export default HomePage
+            gap: "20px",
+          }}
+        >
+          <WrapperProducts>
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+          </WrapperProducts>
+        </div>
+        
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
